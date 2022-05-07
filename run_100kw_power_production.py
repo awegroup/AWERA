e96319e8e8f0af339484311320b85fb90175761d
lthUniBonn <lthimm@uni-bonn.de>
2022-05-07 21:36:26 +0200

    add scan

--- run_100kw_power_production.py
+++ run_100kw_power_production.py
@@ -13,20 +13,21 @@ config = Config()
 import os
 
 sel_types = [
-    # 10m
-    [0, 0, 0],
-    [0, 1, 0],
-    [1, 1, 0],
+    # # 10m
+    # [0, 0, 0],
+    # [0, 1, 0],
+    # [1, 1, 0],
     # 100m
-    [0, 0, 1],
+    # [0, 0, 1],
     [0, 1, 1],
     [1, 1, 1]
     ]
-do_settings_scan = True
+do_settings_scan = False  # True
 read_curve = True
+use_phase_eff = False  # True
 if do_settings_scan:
     # Scan kitepower 100kW system
-    SEL_ID = 4  # 1 or 4 for 10m or 100m ref
+    SEL_ID = 0  # 100kW or 500kW 10m or 100m ref
     SCAN_SEL = int(os.environ['SEL_ID'])
 else:
     SEL_ID = int(os.environ['SEL_ID'])
@@ -53,28 +54,39 @@ if kite == '100':
 elif 'kitepower' in kite:
     # Tether between 200 and 500m
     bounds = [None, None, None, [150, 300], [200, 250]]
-    x0 = [8000., 1000., 0.5, 300., 200.0]
-    # x0 = [45000., 8000., 0.5, 300., 200.0]
+    x0 = [25000., 1000., 0.5, 230., 200.0]
 
 
 if do_settings_scan:
     scan_opts = [
-        [500, [2, 10], 110, 500, [25, 60]],  # Default
-        [250, [2, 10], 110, 500, [25, 60]],  # low_min_ft
-        [500, [0.5, 10], 110, 500, [25, 60]],  # low_min_vr
-        [500, [2, 10], 90, 500, [25, 60]],  # course_ang_90
-        [500, [2, 10], 110, 700, [25, 60]],  # high_max_dl
-        [500, [2, 10], 110, 500, [20, 60]],  # low_min_elev
-        [500, [2, 10], 110, 500, [25, 75]],  # high_max_elev
+        [[500, None], [0.05, [10, 5]], 110, 500, [25, 60]],  # diff_max_vr  #  in out
+        [[500, 78000], [0.05, [10, 5]], 110, 500, [25, 60]],  # diff_max_vr_kitepower  #  in out
+        [[500, None], [2, 10], 110, 500, [25, 60]],  # Default
+        [[500, None], [0.05, 5], 110, 500, [25, 60]],  # low_min_max_vr
+        [[500, 78000], [0.05, 5], 110, 500, [25, 60]],  # kitepower
+        [[500, None], [0.5, 10], 110, 500, [25, 60]],  # low_min_vr
+        [[500, None], [0.000001, 10], 110, 500, [25, 60]],  # zero_min_vr
+        [[500, None], [2, 20], 110, 500, [25, 60]],  # high_max_vr
+        [[500, None], [2, 10], 90, 500, [25, 60]],  # course_ang_90
+        [[500, None], [2, 10], 110, 700, [25, 60]],  # high_max_dl
+        [[500, None], [2, 10], 110, 500, [20, 60]],  # low_min_elev
+        [[500, None], [2, 10], 110, 500, [25, 75]],  # high_max_elev
+        [[250, None], [2, 10], 110, 500, [25, 60]],  # low_min_ft
         ]
     scan_tags = [
+        'diff_max_vr',
+        'diff_max_vr_kitepower',
         'default',
-        'low_min_ft',
+        'low_min_max_vr',
+        'kitepower',
         'low_min_vr',
+        'zero_min_vr',
+        'high_max_vr',
         'course_ang_90',
         'high_max_dl',
         'low_min_elev',
-        'high_max_elev'
+        'high_max_elev',
+        'low_min_ft',
         ]
     scan_tag = scan_tags[SCAN_SEL] + '_'
     scan_opt = scan_opts[SCAN_SEL]
@@ -82,9 +94,15 @@ if do_settings_scan:
     # Settings scan
     # System:
     # min tether force, reeling speed limits, course angle
-    sys_props['tether_force_min_limit'] = scan_opt[0]
+    sys_props['tether_force_min_limit'] = scan_opt[0][0]
+    if scan_opt[0][1] is not None:
+        sys_props['tether_force_max_limit'] = scan_opt[0][1]
     sys_props['reeling_speed_min_limit'] = scan_opt[1][0]
-    sys_props['reeling_speed_max_limit'] = scan_opt[1][1]
+    if type(scan_opt[1][1]) == list:
+        sys_props['reeling_speed_max_limit_retr'] = scan_opt[1][1][0]
+        sys_props['reeling_speed_max_limit_trac'] = scan_opt[1][1][1]
+    else:
+        sys_props['reeling_speed_max_limit'] = scan_opt[1][1]
 
     settings['traction']['course_angle'] = scan_opt[2] * np.pi / 180.
 
@@ -98,6 +116,12 @@ if do_settings_scan:
 else:
     scan_tag = ''
 
+if use_phase_eff:
+    scan_tag += 'phase_eff_'
+    # TODO define within settings style?
+    # TODO depending on generator properties and reeling speed
+    settings['phase_efficiencies'] = {'traction': 0.9,
+                                      'retraction': 1/0.65}
 ref_height_sel = [10, 100]
 ref_height = ref_height_sel[sel[2]]  # m
 
@@ -105,13 +129,14 @@ wind_speeds_sel = {10: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
                         13, 14, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19,
                         19.5, 20, 21],
                    100:  # [11, 12, 13],
-                       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-                        13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-                        24, 25, 26, 27, 28]
+                         # [1, 2,
+                         [3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                          13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                          24]  # , 25, 26, 27, 28]
                    }  # m/s
 wind_speeds = wind_speeds_sel[ref_height]
 
-
+# scan_tag = '78_kN_' + scan_tag
 config.update({'General': {'ref_height': ref_height},
                'Power': {'profile_type': '{}kW_log_profile'.format(
                    rated_power),
@@ -137,6 +162,7 @@ config.update({'General': {'ref_height': ref_height},
                            }
                        }
                })
+print(config.IO.plot_output)
 setattr(config.IO, 'training_plot_output', config.IO.plot_output)
 print(config)
 
@@ -159,6 +185,11 @@ if read_curve:
     env_state = LogProfile()
     env_state.set_reference_height(ref_height)
     env_state.set_reference_wind_speed(wind_speeds[0])
+    env_state.plot_wind_profile(color='#2ca02c')
+    plt.xlim([-2.2, 3.2])
+    plt.ylim([0, 600])
+    plt.savefig(prod.config.IO.plot_output.format(
+        title='log_wind_profile_shape'))
     oc = prod.create_optimizer(env_state, wind_speeds[0],
                                sys_props=sys_props,
                                cycle_sim_settings=settings,
@@ -199,13 +230,13 @@ pc.plot_optimal_trajectories(plot_info='_profile_log',
                              circle_radius=
                              settings['cycle']['tether_length_end_retraction'])
 
-pc.plot_optimization_results(oc.OPT_VARIABLE_LABELS,
-                             oc.bounds_real_scale,
-                             [sys_props['tether_force_min_limit'],
-                              sys_props['tether_force_max_limit']],
-                             [sys_props['reeling_speed_min_limit'],
-                              sys_props['reeling_speed_max_limit']],
-                             plot_info='_profile_log')
+# pc.plot_optimization_results(oc.OPT_VARIABLE_LABELS,
+#                              oc.bounds_real_scale,
+#                              [sys_props['tether_force_min_limit'],
+#                               sys_props['tether_force_max_limit']],
+#                              [sys_props['reeling_speed_min_limit'],
+#                               sys_props['reeling_speed_max_limit']],
+#                              plot_info='_profile_log')
 
 n_cwp = np.array([kpis['n_crosswind_patterns']
                   for kpis in pc.performance_indicators]
@@ -221,7 +252,7 @@ prod.compare_kpis([pc])
 prod.plot_trajectories(pc)
 prod.plot_power_curves(pcs=pc, labels='{}kW log profile'.format(rated_power),
                        lim=[wind_speeds[0], wind_speeds[-1]],
-                       speed_at_op_height=True)
+                       speed_at_op_height=True)  # False)
 with open(prod.config.IO.plot_output.format(
         title='power_curve_constructor_{}kw_log_profile_opt'.format(rated_power))
         .replace('.pdf', '.pickle'), 'wb')\
