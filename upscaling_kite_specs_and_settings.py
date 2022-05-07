66065fd7d5e364a2a7f7738bbdf8148af82aff67
lthUniBonn <lthimm@uni-bonn.de>
2022-05-07 21:37:15 +0200

    include improved settings

--- upscaling_kite_specs_and_settings.py
+++ upscaling_kite_specs_and_settings.py
@@ -12,8 +12,10 @@ sys_props_100 = {
     'kite_drag_coefficient_powered': .13,  # [-]
     'kite_lift_coefficient_depowered': .2,  # [-]
     'kite_drag_coefficient_depowered': .1,  # [-] - in the range of .1 - .2
-    'reeling_speed_min_limit': 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
-    'reeling_speed_max_limit': 10,  # [m/s]
+    'reeling_speed_min_limit': 0.05,  # 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
+    'reeling_speed_max_limit_retr': 10,
+    'reeling_speed_max_limit_trac': 5,
+    # 'reeling_speed_max_limit': 10,  # [m/s]
     'tether_drag_coefficient': 1.1,  # [-]
 }
 
@@ -51,18 +53,20 @@ settings_100 = {
 # Kitepower 100kW
 sys_props_kitepower_100 = {
     # TODO surface area
-    'kite_projected_area': 100,  # [m^2] - of ??? total flat area
+    'kite_projected_area': 78,  # 100m^2 flat area, estimated by reduction of 1.28,  # [m^2] - of ??? total flat area
     'kite_mass': 100 + 70,  # [kg] - kite + KCU
     'tether_density': 724.,  # [kg/m^3] - 0.85 GPa
-    'tether_diameter': 0.014,  # [m]
-    'tether_force_max_limit': 5000 * 9.81,  #5000/(.25*np.pi*0.004**2) * (.25*np.pi*0.014**2),  # ~61250 [N]
+    'tether_diameter': 0.017,  # 0.014,  # [m]
+    'tether_force_max_limit': 78000,  # 5000 * 9.81,  #5000/(.25*np.pi*0.004**2) * (.25*np.pi*0.014**2),  # ~61250 [N]
     'tether_force_min_limit': 500,  # [N]
     'kite_lift_coefficient_powered': 1.05,  # [-]
     'kite_drag_coefficient_powered': .13,  # [-]
     'kite_lift_coefficient_depowered': .2,  # [-]
     'kite_drag_coefficient_depowered': .1,  # [-] - in the range of .1 - .2
-    'reeling_speed_min_limit': 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
-    'reeling_speed_max_limit': 10,  # [m/s]
+    'reeling_speed_min_limit': 0.05,  # 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
+    'reeling_speed_max_limit_retr': 10,
+    'reeling_speed_max_limit_trac': 5,
+    # 'reeling_speed_max_limit': 10,  # [m/s]
     'tether_drag_coefficient': 1.1,  # [-]
 }
 
@@ -97,18 +101,20 @@ settings_kitepower_100 = {
 sys_props_kitepower_500 = {
     # TODO surface area
     # TODO tether settings
-    'kite_projected_area': 250,  # [m^2] - of ??? total flat area
+    'kite_projected_area': 199,  # 250m^2 flat area, estimated by reduction of 1.28 (Kitepower Falcon: 60 -> 47) ,  # [m^2]
     'kite_mass': 300 + 70,  # [kg] - kite + KCU
     'tether_density': 724.,  # [kg/m^3] - 0.85 GPa
-    'tether_diameter': 0.014,  # [m]
-    'tether_force_max_limit': 5000 * 9.81,  #5000/(.25*np.pi*0.004**2) * (.25*np.pi*0.014**2),  # ~61250 [N]
+    'tether_diameter': np.sqrt(5) * 0.014,  # [m]
+    'tether_force_max_limit': 5 * 5000 * 9.81,  #5000/(.25*np.pi*0.004**2) * (.25*np.pi*0.014**2),  # ~61250 [N]
     'tether_force_min_limit': 500,  # [N]
     'kite_lift_coefficient_powered': 1.1,  # [-]
     'kite_drag_coefficient_powered': .12,  # [-]
     'kite_lift_coefficient_depowered': .2,  # [-]
     'kite_drag_coefficient_depowered': .1,  # [-] - in the range of .1 - .2
-    'reeling_speed_min_limit': 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
-    'reeling_speed_max_limit': 10,  # [m/s]
+    'reeling_speed_min_limit': 0.05,  # 2,  # [m/s] - ratio of 4 between lower and upper limit would reduce generator costs
+    'reeling_speed_max_limit_retr': 10,
+    'reeling_speed_max_limit_trac': 5,
+    # 'reeling_speed_max_limit': 10,  # [m/s]
     'tether_drag_coefficient': 1.1,  # [-]
 }
 
